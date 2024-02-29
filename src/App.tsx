ceaaa7123933350e0e466badeb9d2b954841450f
Lordwill1 <messagelordwill@gmail.com>
2024-02-29 17:24:25 +0100

    Showing Alert when you click a button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Button color="primary" onClick={() => console.log('Clicked')}>My Button</Button>
+      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -2,11 +2,15 @@ import React, { ReactNode } from 'react'
 
 interface Props{
     children: ReactNode;
+    onClose: () => void;
 }
 
-const Alert = ( { children }: Props) => {
+const Alert = ( { children, onClose }: Props) => {
   return (
-    <div className="alert alert-primary">{children}</div>
+    <div className="alert alert-primary alert-dismissible">
+        {children}
+        <button type="button" className="btn-close" onClick={onClose} data-bs-dismiss ="alert"></button>
+        </div>
   )
 }
 
