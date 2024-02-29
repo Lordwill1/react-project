af1f6ed8189cd7e1ac14721893bbd679d4c4d869
Lordwill1 <messagelordwill@gmail.com>
2024-02-29 14:42:17 +0100

    Building button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div>
-      <Alert>
-        Hello <span>World</span>
-      </Alert>
+      <Button color="primary" onClick={() => console.log('Clicked')}>My Button</Button>
     </div>
   );
 }
