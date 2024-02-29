773beee2a3e9063f504a0c7e1f65ba28401c15f4
Lordwill1 <messagelordwill@gmail.com>
2024-02-29 16:51:24 +0100

    Building button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 interface Props {
     children: string;
-    color: string;
+    color?: 'primary' | 'secondary' | 'danger';
     onClick: () => void;
 }
 
-const Button = ({ children, onClick, color }: Props) => {
+const Button = ({ children, onClick, color = 'primary' }: Props) => {
   return (
     <button className={'btn btn-' +color} onClick={onClick}>{children}</button>
   )
